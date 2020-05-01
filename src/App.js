15e586393d6f27fb118b75347a9821de4035000c
Andreas Nyström <andreas.nystrom@tretton37.com>
2020-05-01 12:27:55 +0200

    Add base header/main/footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h2>A fine presentation of cypress - huh</h2>
       </header>
+      <main className='App-main'>
+        <h1>This is for testing</h1>
+        <input className='input1' value={inputValue} onChange={e => setInputValue(e.currentTarget.value)}></input>
+      </main>
+      <footer className='App-footer'>
+        <p>A mighty fine footer</p>
+        <p>With some info here too</p>
+
+      </footer>
     </div>
   );
 }
